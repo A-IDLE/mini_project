010052a03ff89b2665fc5a699e544523fb3498a0
1017toa <1017toa@naver.com>
2024-04-18 13:02:38 +0900

    DB캐싱 요약까지 완료

--- app/routers/routers.py
+++ app/routers/routers.py
@@ -21,13 +21,23 @@ def get_db():
         db.close()
 
 @router.post("/summary")
-async def summary_news_route(request: Request):
+async def summary_news_route(request: Request,db: Session = Depends(get_db)):
     # 요청 본문에서 텍스트 추출
     data = await request.json()
     newsChunk = data["text"]
+    link = data["link"]
 
-    # 요약 함수 실행
-    summary = await summarize_news(newsChunk)
+    # 해당 뉴스 요약이 이미 있는지 확인합니다.
+    existing_news = db.query(News).filter(News.link == link).first()
+    if existing_news.summarized_text:
+        print("요약 있다")
+        summary = existing_news.summarized_text
+    else:
+        # 요약 함수 실행
+        summary = await summarize_news(newsChunk)
+        existing_news.summarized_text = summary
+    # 변경사항을 커밋합니다.
+    db.commit()
 
     print(summary)
 
@@ -53,15 +63,15 @@ async def search_news(request: Request,db: Session = Depends(get_db)):
 
     # 요약 함수 실행
     search_result = await crawling(srcText, srcCnt)
-    
+
     for article in search_result:
         # 해당 뉴스가 이미 있는지 확인합니다.
         existing_news = db.query(News).filter(News.link == article['link']).first()
         if existing_news:
-            print(article['title'])
+            print(article['emotion'])
         else:
             # news 테이블에 새로운 레코드를 추가합니다.
-            add_news = News(title=article['title'], description=article['description'], org_link=article['org_link'], link=article['link'], full_text=article['full_text'], keyword=srcText)
+            add_news = News(title=article['title'], description=article['description'], org_link=article['org_link'], link=article['link'], full_text=article['full_text'][:10000], keyword=srcText, emotion=article['emotion'], postive_percent=article['postive_percent'], negative_percent=article['negative_percent'])
             db.add(add_news)
     # 변경사항을 커밋합니다.
     db.commit()
